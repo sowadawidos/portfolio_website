ab21df9abdbb34627765f2b60f79cfb05eaf3433
Dawid Sowinski <sowadawidos@wp.pl>
2021-07-02 13:54:12 +0200

    test

--- src/components/About_us/AboutUsMain.js
+++ src/components/About_us/AboutUsMain.js
@@ -9,7 +9,7 @@ export const AboutUsMain = () => {
             <div className="about__us-main">
                 <h1 className="about__us-title">O nas</h1>
                 <img src={decoration} alt="decoration" className="decoration"/>
-                <p className="about__us-text">Nori grape silver beet broccoli kombu beet greens
+                <p className="about__us-text">Noris grape silver beet broccoli kombu beet greens
                     fava bean potato quandong celery. Bunya nuts black-eyed pea prairie
                     turnip leek
                     lentil turnip greens parsnip.
